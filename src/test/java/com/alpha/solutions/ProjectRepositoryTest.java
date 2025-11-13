eff10485107793b05eedb31ac1fbd1744c8b47c6
Gotze <thorholby@gmail.com>
2025-11-13 13:36:33 +0100

    H2 Setup

---
+++ src/test/java/com/alpha/solutions/ProjectRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.alpha.solutions;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(scripts = "classpath:h2init.sql", executionPhase = BEFORE_TEST_METHOD)
+public class ProjectRepositoryTest {
+
+}
