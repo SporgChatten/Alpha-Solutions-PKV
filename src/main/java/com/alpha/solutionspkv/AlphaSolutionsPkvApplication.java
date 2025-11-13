9d64e1fae63e888810442a17c70c045df43ef52f
Gotze <thorholby@gmail.com>
2025-11-13 12:25:05 +0100

    Spring Boot setup

---
+++ src/main/java/com/alpha/solutionspkv/AlphaSolutionsPkvApplication.java
@@ -0,0 +1,11 @@
+package com.alpha.solutionspkv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionsPkvApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSolutionsPkvApplication.class, args);
+    }
+}
