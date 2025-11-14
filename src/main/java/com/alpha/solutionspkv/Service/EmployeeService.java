89cbde18128f7ac9eb05cb58257a73045f5558be
CodeSpitterKEa <olivervinstedi@gmail.com>
2025-11-14 13:32:56 +0100

    Service wrapping and repo changes

--- src/main/java/com/alpha/solutionspkv/Service/EmployeeService.java
+++ src/main/java/com/alpha/solutionspkv/Service/EmployeeService.java
@@ -1,8 +1,31 @@
 package com.alpha.solutionspkv.Service;
 
+import com.alpha.solutionspkv.Model.Employee;
+import com.alpha.solutionspkv.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
 
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee findById(int id) {
+        return employeeRepository.findById(id);
+    }
+
+    public void create(Employee employee) { employeeRepository.create(employee); }
+
+    public void update(Employee employee) { employeeRepository.update(employee); }
+
+    public void delete(int employeeId) { employeeRepository.delete(employeeId); }
 }
--- src/main/java/com/alpha/solutionspkv/Service/ProjectService.java
+++ src/main/java/com/alpha/solutionspkv/Service/ProjectService.java
@@ -1,7 +1,38 @@
 package com.alpha.solutionspkv.Service;
 
+import com.alpha.solutionspkv.Model.Project;
+import com.alpha.solutionspkv.Repository.ProjectRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectService {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(JdbcTemplate jdbcTemplate, ProjectRepository projectRepository) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> findAll() {
+        return projectRepository.findAll();
+    }
+
+    public Project findById(int projectId) {
+        return projectRepository.findById(projectId);
+    }
+
+    public void create(Project project) {
+        projectRepository.create(project);
+    }
+    public void update(Project project) {
+        projectRepository.update(project);
+    }
+    public void delete(int id) {
+        projectRepository.delete(id);
+    }
 }
